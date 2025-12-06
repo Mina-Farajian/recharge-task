2c25261f66f4675cb461020e4aa1032c15af62a8
mina farajian <farajian.mina70@gmail.com>
2025-12-06 18:38:02 +0330

    Fix: fixed issues in tf sections

--- terraform/helm_app.tf
+++ terraform/helm_app.tf
@@ -2,4 +2,5 @@ resource "helm_release" "app" {
   name       = "app"
   chart      = "./charts/"
   namespace  = "dev"
+  create_namespace = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,32 @@ variable "kubeconfig_path" {
 variable "minikube_ip" {
   type = string
 }
+
+variable "istio_chart_repo" {
+  description = "Istio Helm chart repo URL"
+  type        = string
+  default     = "https://istio-release.storage.googleapis.com/charts"
+}
+
+variable "istio_version" {
+  description = "Istio chart version (optional)"
+  type        = string
+  default     = ""
+}
+
+variable "app_image" {
+  description = "Container image for the app"
+  type        = string
+  default     = "recharge-task/app:latest"
+}
+
+variable "app_release_name" {
+  description = "Helm release name for the app"
+  type        = string
+  default     = "my-app"
+}
+
+variable "app_namespace" {
+  type    = string
+  default = "dev"
+}
