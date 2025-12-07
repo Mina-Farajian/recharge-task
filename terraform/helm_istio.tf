6fae249816e6a6ae13c067cf5633df09e127afa4
mina farajian <farajian.mina70@gmail.com>
2025-12-07 13:06:47 +0330

    Fixed: change helm-istio timing

--- terraform/helm_istio.tf
+++ terraform/helm_istio.tf
@@ -18,7 +18,7 @@ resource "helm_release" "istio_base" {
   create_namespace = true
 
   wait    = true
-  timeout = 10
+  timeout = 300
 
   depends_on = [null_resource.add_istio_repo]
 }
@@ -31,7 +31,7 @@ resource "helm_release" "istiod" {
   namespace  = "istio-system"
 
   wait    = true
-  timeout = 10
+  timeout = 300
 
   depends_on = [
     helm_release.istio_base
@@ -48,7 +48,7 @@ resource "helm_release" "istio_ingress" {
   values = [file("${path.module}/istio-values.yaml")]
 
   wait    = true
-  timeout = 10
+  timeout = 300
 
   depends_on = [
     helm_release.istiod
