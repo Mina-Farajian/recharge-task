8c6be718b3ae00ae7a584fa386465a7fc0ccb36a
mina farajian <farajian.mina70@gmail.com>
2025-12-07 14:26:03 +0330

    ops: Refactoring

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -29,10 +29,21 @@ provider "helm" {
 
 #
 provider "aws" {
+  # 1. Credentials (Fake but required for initialization)
+  region                  = "us-east-1"
+  access_key              = "mock_access_key"
+  secret_key              = "mock_secret_key"
+
+  # 2. Skip Validation (Essential for Moto)
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  # 3. Use the Minikube IP variable (must be correct)
   endpoints {
-    ec2        = "http://localhost:5000"
-    elbv2 = "http://localhost:5000" # ALB
-    cloudfront = "http://localhost:5000"
-    wafv2      = "http://localhost:5000"
+    ec2        = "http://${var.minikube_ip}:5000"
+    elbv2      = "http://${var.minikube_ip}:5000"
+    cloudfront = "http://${var.minikube_ip}:5000"
+    wafv2      = "http://${var.minikube_ip}:5000"
   }
 }
