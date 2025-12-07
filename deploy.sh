d69abde52d58757a0bf2803f4b23bd8a1480a0c1
mina farajian <farajian.mina70@gmail.com>
2025-12-07 22:54:57 +0330

    Fix: edit kube folder path in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -88,8 +88,8 @@ kubectl label namespace "$NAMESPACE_APP" istio-injection=enabled --overwrite
 
 # Apply Istio Custom Resources (Gateway and VirtualService)
 echo "Applying Istio Gateway and VirtualService..."
-kubectl apply -f "./k8s/istio-gateway.yaml"
-kubectl apply -f "./k8S/istio-virtualservice.yaml"
+kubectl apply -f "$ROOT/k8s/istio-gateway.yaml"
+kubectl apply -f "$ROOT/k8S/istio-virtualservice.yaml"
 
 # --- 5. APPLICATION DEPLOYMENT (Helm) ---
 echo "Deploying application 'my-app' via Helm..."
