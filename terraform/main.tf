fccc7bd88c85eb0aa2f4170b4a602e9aa35983bc
mina farajian <farajian.mina70@gmail.com>
2025-12-08 11:20:32 +0330

    Fix: cleanup

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,17 +47,3 @@ resource "kubernetes_manifest" "istio_virtualservice" {
   manifest = yamldecode(data.local_file.istio_virtualservice.content)
 }
 
-
-#This tells Terraform:
-#“Hey, read this file and give me its contents as a string.”
-
-# yamldecode(...) converts the YAML string → Terraform map.
-# kubernetes_manifest takes that map and applies it to the cluster.
-# So Terraform won't try to create a Gateway before Istio itself is ready.
-
-#1- Terraform loads your YAML file
-
-#2-Converts it into a manifest
-
-#3-Applies it after Istio ingress gateway is installed
-#custom resource difinitions are not installed by Istio Helm charts, they depend on the application you deploy
